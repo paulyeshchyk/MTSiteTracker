9d911f585a8fd47d710e1a7dd86c0c5315cce53c
Pavel Yeshchyk <Pavel_Yeshchyk@epbygrow0035t.grodno.epam.com>
2014-10-03 15:04:12 +0300

    transparent indicator

---
+++ MTSiteTracker/MTSiteTracker/components/switcher/MaleFemaleSwitcher/MTMaskedSwitcherIndicator.h
@@ -0,0 +1,13 @@
+//
+//  MTMaskedSwitcherIndicator.h
+//  MTSiteTracker
+//
+//  Created by Pavel Yeshchyk on 10/3/14.
+//  Copyright (c) 2014 py. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTMaskedSwitcherIndicator : UIView
+
+@end
---
+++ MTSiteTracker/MTSiteTracker/components/switcher/MaleFemaleSwitcher/MTMaskedSwitcherIndicator.m
@@ -0,0 +1,31 @@
+//
+//  MTMaskedSwitcherIndicator.m
+//  MTSiteTracker
+//
+//  Created by Pavel Yeshchyk on 10/3/14.
+//  Copyright (c) 2014 py. All rights reserved.
+//
+
+#import "MTMaskedSwitcherIndicator.h"
+
+@implementation MTMaskedSwitcherIndicator
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
